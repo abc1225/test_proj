15ad82dd7b990249b254b1ad30e8d34237e43b0c
Jue <xiaojue@huarui.com>
2018-06-30 18:10:22 +0800

    add

--- meicoin_test.sh
+++ meicoin_test.sh
@@ -2,7 +2,7 @@
 
 # set user cookie
 # like "Cookie:__cfduid=da6ba3394d54065b93f0e2362a5b5b5d51529456825; PHPSESSID=h4ha4qecupp8ur4pb2tgnoju30; landlan=1; _hjIncludedInSample=1; PHPSESSID=nhj0t7p4ajv92qok9qkukdtbl0; landlan=1; total=; _hjIncludedInSample=1; _ga=GA1.2.1156639940.1529735184; think_language=zh-CN; userName=15555564455; cookie_username=15555564455; SERVERID=dbd8bb77eb20c19d024afc02e389695a|1530326687|1530266762"
-USER_COOKIE="Cookie:__cfduid=da6ba3394d54065b93f0e2362a5b5b5d51529456825; PHPSESSID=h4ha4qecupp8ur4pb2tgnoju30; landlan=1; _hjIncludedInSample=1; PHPSESSID=nhj0t7p4ajv92qok9qkukdtbl0; landlan=1; total=; _hjIncludedInSample=1; _ga=GA1.2.1156639940.1529735184; think_language=zh-CN; userName=15555564455; cookie_username=15555564455; SERVERID=dbd8bb77eb20c19d024afc02e389695a|1530326687|1530266762"
+USER_COOKIE="Cookie:__cfduid=da6ba3394d54065b93f0e2362a5b5b5d51529456825; PHPSESSID=h4ha4qecupp8ur4pb2tgnoju30; landlan=1; _hjIncludedInSample=1; PHPSESSID=nhj0t7p4ajv92qok9qkukdtbl0; landlan=1; total=; _hjIncludedInSample=1; _ga=GA1.2.1156639940.1529735184; _gid=GA1.2.398307308.1530343878; think_language=zh-CN; userName=15555564455; cookie_username=15555564455; SERVERID=41bab0e45c36f679c90e7de7f1e53f3e|1530351765|1530266762"
 
 # /www/server/apache/bin/ab  ab
 AB_PATH=ab
@@ -70,30 +70,39 @@ done
 echo -e "\n\nExcuting $A test:  \n"
 case $A in
     main)
+     echo -e "Test URL is: \n$MAIN_URL \n"
      $AB_PATH -r -c $C -n $N  $MAIN_URL
     ;;
     trade)
+     echo -e "Test URL is: \n$TRADE_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $TRADE_URL
     ;;
     api1)
+     echo -e "Test URL is: \n$API1_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API1_URL
     ;;
     api2)
+     echo -e "Test URL is: \n$API2_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API2_URL
     ;;
     api3)
+     echo -e "Test URL is: \n$API3_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API3_URL
     ;;
     api4)
+     echo -e "Test URL is: \n$API4_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API4_URL
     ;;
     api5)
+     echo -e "Test URL is: \n$API5_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API5_URL
     ;;
     api6)
+     echo -e "Test URL is: \n$API6_URL \n Test Cookie is: \n$USER_COOKIE\n\n"
     $AB_PATH -r -c $C -n $N -H "$USER_COOKIE"  $API6_URL
     ;;
     hello)
+     echo -e "Test URL is: \n$HELLO_URL \n"
      $AB_PATH -r -c $C -n $N  $HELLO_URL
     ;;
 esac
